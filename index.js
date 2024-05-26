fb2f3082c492dfe2a380101ef846337945ce75d9
camsilicious <152408636+camsilicious@users.noreply.github.com>
2024-05-26 18:18:10 +0800

    Update index.js

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ function countLike1() {
   let likeCounts = parseInt(count1.value) + 1
   count1.textContent = likeCounts.toString()
 }
+
 btnLike1.addEventListener("click", countLike1)
 
 const btnLike2 = document.getElementById("btnLike2")
@@ -14,15 +15,17 @@ function countLike2() {
   let likeCounts = parseInt(count2.value) + 1
   count2.textContent = likeCounts.toString()
 }
+
 btnLike2.addEventListener("click", countLike2)
 
 const btnDislike1 = document.getElementById("btnDislike1")
 const count3 = document.getElementById("count3")
 
-function btnDislike1() {
+function countDislike1() {
   let DislikeCounts = parseInt(count3.value) + 1
   count3.textContent = DislikeCounts.toString()
 }
+
 btnDislike1.addEventListener("click", countDislike1)
 
 const btnDislike2 = document.getElementById("btnDislike2")
@@ -32,4 +35,5 @@ function countDislike2() {
   let DislikeCounts = parseInt(count4.value) + 1
   count4.textContent = DislikeCounts.toString()
 }
+
 btnDislike2.addEventListener("click", countDislike2)
