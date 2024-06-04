8fb6f3eef63af402b62b8a7473fb51e0dbf26a3c
camsilicious <152408636+camsilicious@users.noreply.github.com>
2024-06-04 19:38:18 +0800

    Update index.js

--- index.js
+++ index.js
@@ -2,38 +2,35 @@ const btnLike1 = document.getElementById("btnLike1")
 const count1 = document.getElementById("count1")
 
 function countLike1() {
-  let likeCounts = parseInt(count1.value) + 1
-  count1.textContent = likeCounts.toString()
+  let likeCounts = parseInt(count1.innerText) + 1
+  count1.innerText = likeCounts.toString()
 }
-
 btnLike1.addEventListener("click", countLike1)
 
-const btnLike2 = document.getElementById("btnLike2")
+const btnDislike1 = document.getElementById("btndislike1")
+const dcount1 = document.getElementById("dcount1")
+
+function countDislike1() {
+  let dislikeCounts = parseInt(dcount1.innerText) + 1
+  dcount1.innerText = dislikeCounts.toString()
+}
+btnDislike1.addEventListener("click", countDisLike1)
+
+const btnLike2 = document.getElementById("btnlike2")
 const count2 = document.getElementById("count2")
 
 function countLike2() {
-  let likeCounts = parseInt(count2.value) + 1
-  count2.textContent = likeCounts.toString()
+  let likeCounts = parseInt(count2.innerText) + 1
+  count2.innerText = likeCounts.toString()
 }
-
 btnLike2.addEventListener("click", countLike2)
 
-const btnDislike1 = document.getElementById("btnDislike1")
-const count3 = document.getElementById("count3")
-
-function countDislike1() {
-  let DislikeCounts = parseInt(count3.value) + 1
-  count3.textContent = DislikeCounts.toString()
-}
-
-btnDislike1.addEventListener("click", countDislike1)
-
-const btnDislike2 = document.getElementById("btnDislike2")
-const count4 = document.getElementById("count4")
+const btnDislike2 = document.getElementById("btndislike2")
+const dcount2 = document.getElementById("dcount2")
 
 function countDislike2() {
-  let DislikeCounts = parseInt(count4.value) + 1
-  count4.textContent = DislikeCounts.toString()
+  let dislikeCounts = parseInt(dcount2.innerText) + 1
+  dcount2.innerText = dislikeCounts.toString()
 }
 
 btnDislike2.addEventListener("click", countDislike2)
