ccac4bf0d9bc3e225831d5b692ee1b33dea056aa
camsilicious <152408636+camsilicious@users.noreply.github.com>
2024-06-30 10:45:07 +0800

    Update app.js

--- app.js
+++ app.js
@@ -1,73 +1,132 @@
-<!DOCTYPE html>
-  <html lang="EN"/>
-   <title>My Homepage</title>
-    <head>
-      <meta charset="UTF-8">
-      <meta name="viewport" content="width=device-width,initial-scale=1.0">
-      <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-      <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
-    </head>
-    <body> 
-    <div class="container">
-      <div class="row">
-         <div class="col-12 text-center">
-           <h2>LUXE FRAGRANCES</h2>
-           <hr class="hr"/>
-         </div>
-        <div class="row">
-      <div class="col-4 text-center">
-        <label id="product1"> Secret Desire </label>
-        <img src="secret desire.png" style="width:400px;height:500px;" class="img-thumbnail"/><br>
-         <label for="product1" id="price1"> 299.00 </label>
-        <input type="number" class="form-control" id="qty1" placeholder="Enter Quantity"/><br>
-      </div>
-      
-        <div class="col-4 text-center">
-        <label id="product2"> Sweet Serenity </label>
-        <img src="sweet serenity.png" style="width:400px;height:500px;" class="img-thumbnail"/><br>
-        <label for="product2" id="price2"> 299.00 </label>
-        <input type="number" class="form-control" id="qty2" placeholder="Enter Quantity"/><br>
-        </div>
+// Define variables for product details
+const product1 = {
+  name: "Secret Desire",
+  price: 299.00,
+  qtyInput: document.getElementById("qty1"),
+  priceDisplay: document.getElementById("price1")
+};
 
-        <div class="col-4 text-center">
-        <label id="product3"> Angel's Dust </label>
-        <img src="angels dust.png" style="width:400px;height:500px;" class="img-thumbnail"/><br>
-        <label for="product3" id="price3"> 199.00 </label>
-        <input type="number" class="form-control" id="qty3" placeholder="Enter Quantity"/><br>
-        </div>
+const product2 = {
+  name: "Sweet Serenity",
+  price: 299.00,
+  qtyInput: document.getElementById("qty2"),
+  priceDisplay: document.getElementById("price2")
+};
 
-        <div class="col-4 text-center">
-        <label id="product4"> Love Luna </label>
-        <img src="love luna.png" style="width:400px;height:500px;" class="img-thumbnail"/><br>
-        <label for="product4" id="price4"> 199.00 </label>
-        <input type="number" class="form-control" id="qty4" placeholder="Enter Quantity"/><br>
-        </div>
+const product3 = {
+  name: "Angel's Dust",
+  price: 199.00,
+  qtyInput: document.getElementById("qty3"),
+  priceDisplay: document.getElementById("price3")
+};
 
-        <div class="col-4 text-center">
-        <label id="product5"> Floral Musk </label>
-        <img src="floral musk.jpg" style="width:400px;height:500px;" class="img-thumbnail"/><br>
-        <label for="product5" id="price5"> 199.00 </label>
-        <input type="number" class="form-control" id="qty5" placeholder="Enter Quantity"/><br>
-        </div>
+const product4 = {
+  name: "Love Luna",
+  price: 199.00,
+  qtyInput: document.getElementById("qty4"),
+  priceDisplay: document.getElementById("price4")
+};
 
-        <div class="col-4 text-center">
-        <label id="product6"> Dreamy Bliss </label>
-        <img src="dreamy bliss.png" style="width:400px;height:500px;" class="img-thumbnail"/><br>
-        <label for="product6" id="price6"> 299.00 </label>
-        <input type="number" class="form-control" id="qty6" placeholder="Enter Quantity"/><br>
-        </div>
-          
-        <div class="col-4"> 
-           <label for="carts"> Orders </label><br>
-          <textarea class="form-control" rows="29" id="carts" cols="200" readonly style="width:350px;height: 400;font-size: 12px;"> </textarea>"
-          <input type="text" class="form-control" border-0 id="total" readonly placeholder="Total"/>
-          <input type="number" class="form-control" id="cash" placeholder="Cash Tendered"/>
-          <input type="text" class="form-control" border-0 id="change" readonly placeholder="Change"/>
-        </div>
-      </div>
-    </div>
-        <script src="app.js"></script>
-    </body>
-    
-</html>
+const product5 = {
+  name: "Floral Musk",
+  price: 199.00,
+  qtyInput: document.getElementById("qty5"),
+  priceDisplay: document.getElementById("price5")
+};
+
+const product6 = {
+  name: "Dreamy Bliss",
+  price: 299.00,
+  qtyInput: document.getElementById("qty6"),
+  priceDisplay: document.getElementById("price6")
+};
+
+// Define variables for displaying cart details
+const carts = document.getElementById("carts");
+const totalDisplay = document.getElementById("total");
+const cashInput = document.getElementById("cash");
+const changeDisplay = document.getElementById("change");
+
+// Function to calculate total cost and change
+function calculateAndDisplay() {
+  // Clear previous cart contents
+  carts.textContent = "";
+
+  let totalCost = 0;
+
+  // Process product 1
+  if (parseFloat(product1.qtyInput.value) > 0) {
+    const qty1 = parseFloat(product1.qtyInput.value);
+    const price1 = product1.price;
+    const subtotal1 = qty1 * price1;
+    carts.textContent += `${qty1} pc/s x ${price1.toFixed(2)} ------ ${product1.name} ------ Php ${subtotal1.toFixed(2)}\n`;
+    totalCost += subtotal1;
+  }
+
+  // Process product 2
+  if (parseFloat(product2.qtyInput.value) > 0) {
+    const qty2 = parseFloat(product2.qtyInput.value);
+    const price2 = product2.price;
+    const subtotal2 = qty2 * price2;
+    carts.textContent += `${qty2} pc/s x ${price2.toFixed(2)} ------ ${product2.name} ------ Php ${subtotal2.toFixed(2)}\n`;
+    totalCost += subtotal2;
+  }
+
+ // Process product 3
+  if (parseFloat(product3.qtyInput.value) > 0) {
+    const qty3 = parseFloat(product3.qtyInput.value);
+    const price3 = product3.price;
+    const subtotal3 = qty3 * price3;
+    carts.textContent += `${qty3} pc/s x ${price3.toFixed(2)} ------ ${product3.name} ------ Php ${subtotal3.toFixed(2)}\n`;
+    totalCost += subtotal3;
+  }
+
+ // Process product 4
+  if (parseFloat(product4.qtyInput.value) > 0) {
+    const qty4 = parseFloat(product4.qtyInput.value);
+    const price4 = product4.price;
+    const subtotal4 = qty4 * price4;
+    carts.textContent += `${qty4} pc/s x ${price4.toFixed(2)} ------ ${product4.name} ------ Php ${subtotal4.toFixed(2)}\n`;
+    totalCost += subtotal4;
+  }
+
+ // Process product 5
+  if (parseFloat(product5.qtyInput.value) > 0) {
+    const qty5 = parseFloat(product5.qtyInput.value);
+    const price5 = product5.price;
+    const subtotal5 = qty5 * price5;
+    carts.textContent += `${qty5} pc/s x ${price5.toFixed(2)} ------ ${product5.name} ------ Php ${subtotal5.toFixed(2)}\n`;
+    totalCost += subtotal5;
+  }
+
+ // Process product 6
+  if (parseFloat(product6.qtyInput.value) > 0) {
+    const qty6 = parseFloat(product6.qtyInput.value);
+    const price6 = product6.price;
+    const subtotal6 = qty6 * price6;
+    carts.textContent += `${qty6} pc/s x ${price6.toFixed(2)} ------ ${product6.name} ------ Php ${subtotal6.toFixed(2)}\n`;
+    totalCost += subtotal6;
+  }
+
+
+  // Display total cost
+  totalDisplay.value = `Total: Php ${totalCost.toFixed(2)}`;
+
+  // Calculate change if cash tendered is provided
+  const cashAmount = parseFloat(cashInput.value);
+  if (!isNaN(cashAmount) && cashAmount >= totalCost) {
+    const changeAmount = cashAmount - totalCost;
+    changeDisplay.value = `Change: Php ${changeAmount.toFixed(2)}`;
+  } else {
+    changeDisplay.value = "Insufficient Cash";
+  }
+}
+
+// Event listeners for input fields to dynamically update calculations
+product1.qtyInput.addEventListener("input", calculateAndDisplay);
+product2.qtyInput.addEventListener("input", calculateAndDisplay);
+product3.qtyInput.addEventListener("input", calculateAndDisplay);
+product4.qtyInput.addEventListener("input", calculateAndDisplay);
+product5.qtyInput.addEventListener("input", calculateAndDisplay);
+product6.qtyInput.addEventListener("input", calculateAndDisplay);
+cashInput.addEventListener("input", calculateAndDisplay);
